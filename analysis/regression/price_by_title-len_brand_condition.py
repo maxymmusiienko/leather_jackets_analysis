bacfb570af8a574717da4f8710e630c5e3c4e4a1
maxmus <maxymvm@gmail.com>
2026-02-05 14:09:21 +0200

    added residuals analysis to lm and also correlation matrix

--- analysis/regression/price_by_title-len_brand_condition.py
+++ analysis/regression/price_by_title-len_brand_condition.py
@@ -1,5 +1,7 @@
 import statsmodels.api as sm
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('C:\\Users\\Musiyenko.M\\PycharmProjects\\leather_jackets_analysis\\analysis\\regression\\cleaned_df.csv')
 
@@ -15,3 +17,16 @@ model = sm.OLS(y, X.astype(float))
 results = model.fit()
 
 print(results.summary())
+
+plt.figure(figsize=(10, 6))
+sns.residplot(x=results.fittedvalues, y=results.resid, lowess=True, line_kws={'color': 'red'})
+plt.title('Residuals vs Fitted Values', fontsize=15)
+plt.xlabel('Predicted Log Price', fontsize=12)
+plt.ylabel('Residuals', fontsize=12)
+plt.show()
+
+plt.figure(figsize=(8, 6))
+correlation_matrix = X.drop('const', axis=1).join(y).corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Correlation Matrix of Regression Features')
+plt.show()
